88d9b47b2a92a0e4b1376680846a6a23469a9ed8
Ruslan Penkovskij <ruslan.penkovskij@gmail.com>
2015-11-11 13:08:34 +0300

    correct code

--- pomodoro.js
+++ pomodoro.js
@@ -1,8 +1,8 @@
 /**
  * Created by home on 11/10/15.
  */
-var arrayWork = [0, 10];
-var arrayRest = [0, 5];
+var arrayWork = [2, 0];
+var arrayRest = [1, 0];
 
 $(document).ready(function() {
 
@@ -19,50 +19,50 @@ $(document).ready(function() {
     }
 
     function timerStart() {
-            var min = $('#min').html();
-            var sec = $('#sec').html();
+        var min = $('#min').html();
+        var sec = $('#sec').html();
 
-            if (sec == 0) {
-                if (min == 0) {
-                    set(arrayRest);
-                    setTimeout(timerRest, 1000);
-                    return false;
-                } else {
-                    min--;
-                    if (min < 10) min = "0" + min;
-                    sec = 59;
-                }
+        if (sec == 0) {
+            if (min == 0) {
+                set(arrayRest);
+                setTimeout(timerRest, 1000);
+                return false;
             } else {
-                sec--;
-                if (sec < 10) sec = "0" + sec;
+                min--;
+                if (min < 10) min = "0" + min;
+                sec = 59;
             }
+        } else {
+            sec--;
+            if (sec < 10) sec = "0" + sec;
+        }
 
-            $('#min').text(min);
-            $('#sec').text(sec);
-            setTimeout(timerStart, 1000);
+        $('#min').text(min);
+        $('#sec').text(sec);
+        setTimeout(timerStart, 1000);
     }
 
     function timerRest() {
-        var min1 = $('#min').html();
-        var sec1= $('#sec').html();
+        var min = $('#min').html();
+        var sec= $('#sec').html();
 
-        if (sec1 == 0) {
-            if (min1 == 0) {
+        if (sec == 0) {
+            if (min == 0) {
                 set(arrayWork);
                 setTimeout(timerStart, 1000);
                 return false;
             } else {
-                min1--;
-                if (min1 < 10) min1 = "0" + min1;
-                sec1 = 59;
+                min--;
+                if (min < 10) min = "0" + min;
+                sec = 59;
             }
         } else {
-            sec1--;
-            if (sec1 < 10) sec1 = "0" + sec1;
+            sec--;
+            if (sec < 10) sec = "0" + sec;
         }
 
-        $('#min').text(min1);
-        $('#sec').text(sec1);
+        $('#min').text(min);
+        $('#sec').text(sec);
         setTimeout(timerRest, 1000);
     }
 
